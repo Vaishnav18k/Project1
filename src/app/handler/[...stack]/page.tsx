c74ca6f1a5b6f42db7b366c1836838311e06c033
Vaishnav18k <149280980+Vaishnav18k@users.noreply.github.com>
2025-06-17 23:39:38 +0530

    done changed div for handler/stack if anything you can edit this

--- src/app/handler/[...stack]/page.tsx
+++ src/app/handler/[...stack]/page.tsx
@@ -2,5 +2,10 @@ import { StackHandler } from "@stackframe/stack";
 import { stackServerApp } from "../../../stack";
 
 export default function Handler(props: unknown) {
-  return <StackHandler fullPage app={stackServerApp} routeProps={props} />;
+  return (
+           <div className="max-w-md mx-auto bg-blue-50/50 backdrop-blur-md rounded-lg shadow-lg overflow-hidden border border-blue-200">
+
+  <StackHandler fullPage app={stackServerApp} routeProps={props} />
+    </div>
+  );
 }
