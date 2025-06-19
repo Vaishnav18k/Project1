a53cb8b1f296b6c42b4b9694325047b9fd38226f
Vaishnav18k <149280980+Vaishnav18k@users.noreply.github.com>
2025-06-20 01:43:44 +0530

    started

---
+++ src/app/LandingComponent/Header.tsx
@@ -0,0 +1,86 @@
+import React from 'react'
+import { useUser } from "@stackframe/stack";
+import { UserButton } from "@stackframe/stack";
+import { useRouter } from "next/navigation";
+
+
+import {
+  Scale,
+  User,
+  Mail,
+  Search,
+  CircleAlert,
+  MessageCircle,
+  Linkedin,
+  Github,
+} from "lucide-react";
+import Link from "next/link";
+
+export function Header() {
+    const user = useUser();
+      const router = useRouter();
+       const handleSignInClick = () => {
+        router.push('/signin');
+        router.push('/dashboard')
+      };
+  return (
+   
+      <header className="bg-white  border-border sticky top-0 z-50 shadow-sm">
+          <div className="container mx-auto px-4 py-4 flex justify-between items-center">
+            <div className="flex items-center space-x-3">
+              <div className="bg-black p-2 rounded-lg">
+                <Scale className="h-6 w-6 text-white" />
+              </div>
+              <Link
+                href="/home"
+                className="text-xl font-bold text-foreground hover:text-primary transition-colors"
+              >
+                Case Alert System
+              </Link>
+            </div>
+            <div className="flex items-center space-x-6">
+              <nav className="hidden md:flex space-x-6">
+                <Link
+                  href="#hero"
+                  className="text-muted-red-400 hover:text-black transition-colors"
+                >
+                  Home
+                </Link>
+                <Link
+                  href="/dashboard"
+                  className="text-muted-foreground hover:text-foreground transition-colors"
+                >
+                  Dashboard
+                </Link>
+                <Link
+                  href="#how-it-works"
+                  className="text-muted-foreground hover:text-foreground transition-colors"
+                >
+                  How It Works
+                </Link>
+                <Link
+                  href="#contact"
+                  className="text-muted-foreground hover:text-foreground transition-colors"
+                >
+                  Contact Us
+                </Link>
+                
+                <UserButton />
+              </nav>
+              <div className="flex items-center space-x-3">
+                {!user && (
+                  <Link
+                    href="/signout"
+                    className="flex items-center justify-center gap-2 whitespace-nowrap text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 bg-black text-slate-50 hover:bg-slate-800 h-9 rounded-md px-3 space-x-2"
+                  >
+                    <User className="h-4 w-4" />
+                    <span>Sign In</span>
+                  </Link>
+                )}
+              </div>
+            </div>
+          </div>
+        </header>
+  )
+}
+
---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,12 @@
+'use client'
+import React from 'react'
+import {Header } from '@/app/LandingComponent/Header'
+export default function dashboard() {
+  return (
+     <div className="min-h-screen bg-background">
+<Header />
+    </div>
+  );
+}
+
+ 
